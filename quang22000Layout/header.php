374f2f5046331b1317523d5c137b0a2eaec2f734
luonghuyquang <luonghuyquang092022@gmail.com>
2023-02-14 15:12:42 +0200

    style

--- quang22000Layout/header.php
+++ quang22000Layout/header.php
@@ -12,8 +12,30 @@
 
     <!--local css-->
   <link rel="stylesheet" href="/quang22000webprog2023/quang22000layout/style.css">
+
+  <script>
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("quangBtn").style.display = "block";
+      } else {
+        document.getElementById("quangBtn").style.display = "none";
+      }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+  </script>
+
 </head>
 <body>
+<button onclick="topFunction()" id="quangBtn" title="Go to top"><i class="fa fa-caret-square-o-up"
+        style="font-size:36px;color:white"></i></button>
   <div class="container" >
     <div class="row">
   <nav class="navbar navbar-expand-lg" style="background-color: rgb(65, 7, 109);" >
