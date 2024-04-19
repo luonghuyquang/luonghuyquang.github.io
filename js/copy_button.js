3b958de0307483b84c1136ebcdf35f482dbcb51c
Quang Luong <luonghuyquang092022@gmail.com>
2024-04-19 18:55:36 +0300

    .

--- js/copy_button.js
+++ js/copy_button.js
@@ -1,25 +1,25 @@
-    // Execute the code when the DOM is ready
-    document.addEventListener('DOMContentLoaded', function() {
-      // Get all <code> elements
-      var codeElements = document.getElementsByTagName("code");
+// Execute the code when the DOM is ready
+document.addEventListener('DOMContentLoaded', function() {
+  // Get all <code> elements
+  var codeElements = document.getElementsByTagName("code");
 
-      // Loop through each <code> element
-      for (var i = 0; i < codeElements.length; i++) {
-        var codeElement = codeElements[i];
+  // Loop through each <code> element
+  for (var i = 0; i < codeElements.length; i++) {
+    var codeElement = codeElements[i];
 
-        // Create clipboard icon element
-        var clipboardIcon = document.createElement("i");
-        clipboardIcon.className = "fa fa-clipboard clipboard-icon";
-        clipboardIcon.title = "Copy to clipboard";
+    // Create clipboard icon element
+    var clipboardIcon = document.createElement("i");
+    clipboardIcon.className = "far fa-clipboard clipboard-icon";
+    clipboardIcon.title = "Copy to clipboard";
 
-        // Append the clipboard icon to the <code> element
-        codeElement.appendChild(clipboardIcon);
+    // Append the clipboard icon to the <code> element
+    codeElement.parentNode.insertBefore(clipboardIcon, codeElement.nextSibling);
 
-        // Initialize Clipboard.js for the current <code> element
-        new ClipboardJS(clipboardIcon, {
-          text: function(trigger) {
-            return trigger.parentNode.textContent.trim();
-          }
-        });
+    // Initialize Clipboard.js for the current <code> element
+    new ClipboardJS(clipboardIcon, {
+      target: function(trigger) {
+        return trigger.previousSibling;
       }
     });
+  }
+});
